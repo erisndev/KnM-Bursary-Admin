a88eceefdc45f19fd4ad87b69e544a59a5469f62
Ferdinand <dev@erisngraduate.com>
2025-07-03 17:57:17 +0200

    03/07

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -11,14 +11,14 @@ const Admin = () => {
 
   return (
     <div>
-      <nav className="flex justify-between items-center p-4 bg-gray-100">
+      <nav className="flex flex-col sm:flex-row justify-between items-center p-4 bg-gray-100 gap-4 sm:gap-0">
         <img src=".\Logo.png" alt="Logo" className="w-32" />
-        <h2 className="text-3xl font-bold text-cyan-900 mb-2">
+        <h2 className="text-2xl sm:text-3xl font-bold text-cyan-900 mb-2 sm:mb-0 text-center">
           Admin Dashboard
         </h2>
         <button
           onClick={handleLogout}
-          className="px-4 py-2 bg-cyan-800 text-white rounded hover:bg-cyan-600 transition"
+          className="px-4 py-2 bg-cyan-800 text-white rounded hover:bg-cyan-600 transition w-full sm:w-auto"
         >
           Logout
         </button>
